71f8951bfea52a7be0640813884a5bd1a4919a13
fadelkaadan <fadelkaadan@gmail.com>
2020-04-22 22:29:53 +0200

    Updates

---
+++ precache-manifest.d2a4332b4a363c497935a2e65035ad4a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bfdd7232f74f6c1cc99ea4975729d0",
+    "url": "/perosnal-website/index.html"
+  },
+  {
+    "revision": "f45641661e85e3f5cc32",
+    "url": "/perosnal-website/static/css/main.1608f05d.chunk.css"
+  },
+  {
+    "revision": "68cb2c91bfc0c8025523",
+    "url": "/perosnal-website/static/js/2.453631f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/perosnal-website/static/js/2.453631f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45641661e85e3f5cc32",
+    "url": "/perosnal-website/static/js/main.7f1c93de.chunk.js"
+  },
+  {
+    "revision": "cdfb83f49bb00ac67ae9",
+    "url": "/perosnal-website/static/js/runtime-main.16d5eb9f.js"
+  },
+  {
+    "revision": "d4ab463a28d199315a192408b5c5144a",
+    "url": "/perosnal-website/static/media/bg.d4ab463a.jpg"
+  },
+  {
+    "revision": "529c524d8fb7c0915bb6001902d71136",
+    "url": "/perosnal-website/static/media/book-1.529c524d.jpg"
+  },
+  {
+    "revision": "67cdb757ba7dc3f08350b9a2fadbd8af",
+    "url": "/perosnal-website/static/media/book-2.67cdb757.jpg"
+  },
+  {
+    "revision": "2aa99e05abd4e5beab82b46363969eaf",
+    "url": "/perosnal-website/static/media/book-3.2aa99e05.jpg"
+  },
+  {
+    "revision": "673f8550014c4f0ea88483d1e00933bf",
+    "url": "/perosnal-website/static/media/rocket.673f8550.svg"
+  }
+]);
