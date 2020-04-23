eca321ac8f272be9c1c5dcd0f457ec1e86a9b687
fadelkaadan <fadelkaadan@gmail.com>
2020-04-23 18:53:04 +0200

    imporve footer and bookshelf responsiveness

--- src/components/Bookshelf/Book/Book.js
+++ src/components/Bookshelf/Book/Book.js
@@ -8,8 +8,8 @@ const Book = (props) => {
         <div className={`book ${bookClassName}}`}>
             <div className={`book__side book__side--front ${cover}`}></div>
             <div className={`book__side book__side--back ${backcover}`}>
-                <h3>&#9733;&#9733;&#9733;&#9733;&#9733;</h3>
-                <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iure natus delectus accusamus soluta voluptatem nam, corrupti porro nostrum veniam odit sint nihil esse minima temporibus quis iste? Aliquid, dolor?"</p>
+                <h3 className="rating">&#9733;&#9733;&#9733;&#9733;&#9733;</h3>
+                <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iure natus delectus accusamus soluta voluptatem nam Aliquid, dolor?"</p>
             </div>
         </div>
     )
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,7 +7,7 @@ const Main = (props) => {
         <div className="main">
             <div className="main__content">
                 <h1>Version 2.0</h1>
-                <h2 className="swoosh">This website is not finished and not mobile compatible yet!</h2>
+                <h2 className="swoosh">Under Construction!</h2>
             </div>
         </div>
     )
